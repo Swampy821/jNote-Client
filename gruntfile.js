f7c9e467cd37dd684376dc61244e01bd48db7e64
Steve Marsh <Zimdale@gmail.com>
2014-09-24 11:55:09 -0400

    Got the build system in place, switched to less, cleaned stuff up.

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,27 @@
 module.exports = function (grunt) {   
     grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'),
+      less: {
+          development: {
+            options: {
+              compress: true,
+              yuicompress: true,
+              optimization: 2
+            },
+            files: {
+              "lib/less.gen/bundle.gen.css": "lib/less/*"
+            }
+          }
+        },
+      cssmin: {
+          combine: {
+            files: {
+              'lib/bundle/bundle.min.css': [
+                'lib/less.gen/bundle.gen.css'
+              ]
+            }
+          }
+      },
       uglify: {
         options: {
           mangle: false,
@@ -8,18 +29,20 @@ module.exports = function (grunt) {
         },
         my_target: {
           files: {
-            'lib/js/bundle/bundle.js': [
+            'lib/bundle/bundle.min.js': [
                 'lib/angular/angular.min.js',
                 'lib/angular/angular-route.min.js',
                 'lib/js/app.js',
                 'lib/js/routes.js',
                 'lib/js/objects.js',
-                'lib/js/controllers/homeController.js' 
+                'lib/js/controllers/*.js' 
             ]
           }
         }
       }
     });
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.registerTask('default', ['uglify']);  
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.registerTask('default', ['less', 'uglify', 'cssmin']);  
 };

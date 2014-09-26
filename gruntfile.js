073d1397aceec859777c04c15a24699148b61ccb
Steve Marsh <Zimdale@gmail.com>
2014-09-26 11:30:09 -0400

    Restructured folders and updated build process

--- gruntfile.js
+++ gruntfile.js
@@ -9,15 +9,18 @@ module.exports = function (grunt) {
               optimization: 2
             },
             files: {
-              "lib/less.gen/bundle.gen.css": "lib/less/*"
+              "dev/lib/less.gen/bundle.gen.css": "dev/lib/less/*"
             }
           }
         },
       cssmin: {
           combine: {
             files: {
-              'lib/bundle/bundle.min.css': [
-                'lib/less.gen/bundle.gen.css'
+              'dev/lib/bundle/bundle.min.css': [
+                'dev/lib/less.gen/bundle.gen.css'
+              ],
+              'build/lib/bundle/bundle.min.css': [
+                'dev/lib/bundle/bundle.min.css'
               ]
             }
           }
@@ -29,26 +32,43 @@ module.exports = function (grunt) {
         },
         my_target: {
           files: {
-            'lib/bundle/bundle.min.js': [
-                'lib/angular/angular.min.js',
-                'lib/angular/angular-route.min.js',
-                'lib/angular/angular-cookies.min.js',
-                'lib/js/app.js',
-                'lib/js/routes.js',
-                'lib/js/objects.js',
-                'lib/js/controllers/*.js' 
-            ]
+            'dev/lib/bundle/bundle.min.js': [
+                'dev/lib/angular/angular.min.js',
+                'dev/lib/angular/angular-route.min.js',
+                'dev/lib/angular/angular-cookies.min.js',
+                'dev/lib/js/app.js',
+                'dev/lib/js/routes.js',
+                'dev/lib/js/objects.js',
+                'dev/lib/js/controllers/*.js' 
+            ],
+            'build/lib/bundle/bundle.min.js':[
+            'dev/lib/bundle/bundle.min.js']
           }
         }
       },
       watch: {
         files: ['**/*.less', '**/*.js'],
         tasks: ['less', 'uglify', 'cssmin']
+      },
+      htmlmin: {                                     // Task
+        dist: {                                      // Target
+          options: {                                 // Target options
+            removeComments: true,
+            collapseWhitespace: true
+          },
+          files: {                                   // Dictionary of files
+            'build/index.html': 'dev/index.html',     // 'destination': 'source'
+            'build/lib/partials/home.html': 'dev/lib/partials/home.html',
+            'build/lib/partials/notes.html': 'dev/lib/partials/notes.html'
+          }
+        }
       }
     });
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['less', 'uglify', 'cssmin']);  
+    grunt.loadNpmTasks('grunt-mkdir');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.registerTask('default', ['less', 'uglify', 'cssmin','htmlmin']);  
 };
